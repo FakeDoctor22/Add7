e7b0332d8509f55f8d4a70a690ede80a1874ee4f
FakeDoctor22 <giljoelvillarbaatwork@gmail.com>
2023-05-06 10:36:03 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Write a function called add7 that takes one number and returns the number +7.
+
+const add7 = (number) => {
+    return number + 7;
+};
+
+console.log(add7(0))
+console.log(add7(7))
+console.log(add7(14))
+console.log(add7(21))
+console.log(add7(28))
